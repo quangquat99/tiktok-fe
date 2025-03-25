8c06b21c9c8dc257e05e94f00c238a894ee16537
quangph <quangphuet99@gmail.com>
2025-03-25 14:39:05 +0700

    update redirect

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const TIKTOK_CLIENT_KEY = 'sbawh5jwr50tttl4l9'; // Thay bằng client key của bạn
-const REDIRECT_URI = 'https://portal.eff.vbatechs.com'; // Redirect URI đã đăng ký trên TikTok
+const REDIRECT_URI = 'https://dzew7cvh3rahv.cloudfront.net/callback'; // Redirect URI đã đăng ký trên TikTok
 const SCOPES = 'user.info.basic'; // Chọn scope phù hợp
 
 
